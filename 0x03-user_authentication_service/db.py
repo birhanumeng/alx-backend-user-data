174e6a78942a65a3532e0b41baf705c505e8d402
birhanumeng <eyerusalemmengstie>
2023-06-08 12:31:57 +0000

    add a user to the database

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+""" Database for ORM """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from typing import TypeVar
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
+from typing import TypeVar
 from user import Base
 from user import User
 
 
 class DB:
-    """DB class
-    """
+    """ DB Class for Object Reational Mappin """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+    def __init__(self):
+        """ Constructor Method """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """ Session Getter Method """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
