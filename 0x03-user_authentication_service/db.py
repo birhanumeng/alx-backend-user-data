5bd2405d12cbe9e55e8c31aef2c9c42a9d78dc16
birhanumeng <eyerusalemmengstie>
2023-06-08 12:13:22 +0000

    add user to the database

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from typing import TypeVar
 
 from user import Base
+from user import User
 
 
 class DB:
