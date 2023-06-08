86903fc72358be0dcd0fa3b5bdf78afa236ec010
birhanumeng <eyerusalemmengstie>
2023-06-08 12:22:39 +0000

    add a user to the database

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,40 +1,37 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+""" Database for ORM """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from typing import TypeVar
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
+from typing import TypeVar
 from user import Base
 from user import User
 
 
 class DB:
-    """DB class
-    """
+    """ DB Class for Object Reational Mappin """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+    def __init__(self):
+        """ Constructor Method """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """ Session Getter Method """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add a user to the database and return it.
+        """ Adds user to database
+        Return: User Object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
