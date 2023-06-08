5f9fc55d8a974f3936b44e4fbdf8d2b3c5853f41
birhanumeng <eyerusalemmengstie>
2023-06-08 19:01:40 +0000

    generate hashed password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Authentication Module 
+"""
+
+from bcrypt import hashpw, gensalt, checkpw
+
+
+def _hash_password(password: str) -> str:
+    """ Returns a hashed password for input argument password.
+    """
+    return hashpw(password.encode('utf-8'), gensalt())
