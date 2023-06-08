33e632e0f0d3c767257246fc141d88926c98bd02
birhanumeng <eyerusalemmengstie>
2023-06-08 19:15:27 +0000

    registers a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,8 +4,32 @@
 
 from bcrypt import hashpw, gensalt, checkpw
 
+from db import DB
+
 
 def _hash_password(password: str) -> str:
     """ Returns a hashed password for input argument password.
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ Inialization
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Registering a new user if it is not exit
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
+        else:
+            raise ValueError(f'User {email} already exists')
