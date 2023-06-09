2929d0af4d525aa3c04539f5d353989628955e64
birhanumeng <eyerusalemmengstie>
2023-06-09 15:33:22 +0000

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -31,5 +31,18 @@ def register_users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    email = request.form['email']
+    password = request.form['password']
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = {"email": email, "message": "logged in"}
+        response.set_cookies('session_id', session_id)
+        return response
+    abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
