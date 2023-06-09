a8494ccc389262ececcdfa1f88bed7b42eeff415
birhanumeng <eyerusalemmengstie>
2023-06-09 13:05:27 +0000

    register user from end point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,20 +18,6 @@ def index() -> str:
     payload = {"message": "Bienvenue"}
     return jsonify(payload)
 
-@app.route('/users', method=['POST'])
-def users() -> str:
-    try:
-        email = request.form['email']
-        password = request.form['password']
-    except KeyError:
-        abort(400)
-
-    user = AUTH.register_user(email, password)
-    if user:
-        return jsonify({"email": email, "message": "user created"}), 400
-    else:
-        return jsonify({"message": "email already registered"})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
