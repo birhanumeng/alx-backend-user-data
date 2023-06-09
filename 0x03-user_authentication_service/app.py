6a2298591762489196e367a60ae09d238773600c
birhanumeng <eyerusalemmengstie>
2023-06-09 15:48:05 +0000

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -33,24 +33,28 @@ def register_users() -> str:
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
-    """ Implement the login """
     form_data = request.form
 
     if "email" not in form_data:
         return jsonify({"message": "email required"}), 400
-    if "password" not in form_data:
+    elif "password" not in form_data:
         return jsonify({"message": "password required"}), 400
-
-    email = request.form['email']
-    password = request.form['password']
-
-    if AUTH.valid_login(email, password):
-        session_id = AUTH.create_session(email)
-        response = {"email": email, "message": "logged in"}
-        response.set_cookie('session_id', session_id)
-        return response
     else:
-        abort(401)
+
+        email = request.form.get("email")
+        pswd = request.form.get("password")
+
+        if AUTH.valid_login(email, pswd) is False:
+            abort(401)
+        else:
+            session_id = AUTH.create_session(email)
+            response = jsonify({
+                "email": email,
+                "message": "logged in"
+                })
+            response.set_cookie('session_id', session_id)
+
+            return response
 
 
 if __name__ == "__main__":
