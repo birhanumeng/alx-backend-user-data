41fcd6335d490552664475850b67bf6eadcdaca8
birhanumeng <eyerusalemmengstie>
2023-06-09 15:01:42 +0000

    update the session id of a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,4 +51,15 @@ class Auth:
             user = self._db.find_user_by(email=email)
             return checkpw(password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
-            return False
+            return Falise
+
+    def create_session(self, email: str) -> str:
+        """ Create a session and save it database in user's session_id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
