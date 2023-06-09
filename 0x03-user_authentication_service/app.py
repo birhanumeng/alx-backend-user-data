e3b7b73d0d2fa6416e9caf981e6be05de51febd6
birhanumeng <eyerusalemmengstie>
2023-06-09 15:35:43 +0000

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -39,7 +39,7 @@ def login() -> str:
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
         response = {"email": email, "message": "logged in"}
-        response.set_cookies('session_id', session_id)
+        response.set_cookie('session_id', session_id)
         return response
     abort(401)
 
