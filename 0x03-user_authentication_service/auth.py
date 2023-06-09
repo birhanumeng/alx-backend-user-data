9eea10419afc557089421af90ec2ce1421cb23d6
birhanumeng <eyerusalemmengstie>
2023-06-09 14:00:36 +0000

    check the password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,5 +42,5 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             return checkpw(password.encode('utf-8'), user.hashed_password)
-        except:
+        except NoResultFound:
             return False
