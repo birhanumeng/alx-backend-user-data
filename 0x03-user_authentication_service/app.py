d8e6b9e193b8132bc08a1df8a4329b43519de4cf
birhanumeng <eyerusalemmengstie>
2023-06-10 06:40:14 +0000

    implement the logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ App module
 """
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 
 from auth import Auth
 
@@ -46,6 +46,19 @@ def login() -> str:
         response.set_cookie('session_id', session_id)
         return response
     abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Implement logout
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
