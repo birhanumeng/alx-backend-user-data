9904388d1452faa59b99a7b565cd1b6b47da1504
birhanumeng <eyerusalemmengstie>
2023-06-10 06:49:40 +0000

    implement the logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,16 +49,15 @@ def login() -> str:
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def log_out() -> None:
+def logout():
     """ Implement logout
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        redirect('/')
-    else:
-        abort(403)
+        return redirect('/')
+    abort(403)
 
 
 if __name__ == "__main__":
