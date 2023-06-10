b388704d94ca5876cf7f878ba513e01c58f005dc
birhanumeng <eyerusalemmengstie>
2023-06-10 08:07:44 +0000

    implement profile function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,8 +66,6 @@ def profile() -> str:
         and present in the database.
     """
     session_id = request.cookies.get('session_id')
-    if session_id = None:
-        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({"email": user.email})
