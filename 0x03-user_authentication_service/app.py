b22ac27dbd014f4bf1c0d36a73250dd314a98dce
birhanumeng <eyerusalemmengstie>
2023-06-10 08:04:28 +0000

    implement profile function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,5 +60,19 @@ def logout():
     abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ Alow to access if the user's session id in cookies is valid
+        and present in the database.
+    """
+    session_id = request.cookies.get('session_id')
+    if session_id = None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email})
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
