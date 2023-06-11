d47c7b6477be394f8725adeb2e93ce92af8d8683
birhanumeng <eyerusalemmengstie>
2023-06-11 12:22:33 +0000

    update user password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -94,12 +94,14 @@ def update_password() -> str:
         new_password = request.form['new_password']
     except KeyError:
         abort(400)
-    
+
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
     except ValueError:
         abort(403)
+
+    msg = {"email": email, "message": "Password updated"}
+    return jsonify(msg), 200
 
 
 if __name__ == "__main__":
